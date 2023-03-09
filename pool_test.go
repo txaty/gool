5f8a74361ec2c91d66782b03dcbc02075157d1b1
txaty <xtianae@connect.ust.hk>
2023-03-09 17:43:09 +0800

    Add unit tests.
    Signed-off-by: txaty <xtianae@connect.ust.hk>

--- pool_test.go
+++ pool_test.go
@@ -23,6 +23,7 @@
 package gool
 
 import (
+	"runtime"
 	"testing"
 )
 
@@ -41,11 +42,11 @@ func BenchmarkNormalGoroutine(b *testing.B) {
 }
 
 func BenchmarkPool(b *testing.B) {
-	p := NewPool[interface{}, interface{}](4, 100)
+	p := NewPool[any, any](4, 100)
 	defer p.Close()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		p.Submit(func(interface{}) interface{} {
+		p.Submit(func(any) any {
 			for k := 0; k < 100; k++ {
 				_ = k
 			}
@@ -56,8 +57,8 @@ func BenchmarkPool(b *testing.B) {
 
 func TestPool_Submit(t *testing.T) {
 	type args struct {
-		handler func(interface{}) interface{}
-		args    interface{}
+		handler func(any) any
+		args    any
 	}
 	tests := []struct {
 		name string
@@ -66,7 +67,7 @@ func TestPool_Submit(t *testing.T) {
 		{
 			name: "test",
 			args: args{
-				handler: func(arg interface{}) interface{} {
+				handler: func(arg any) any {
 					for k := 0; k < 100; k++ {
 						_ = k
 					}
@@ -78,7 +79,7 @@ func TestPool_Submit(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			p := NewPool[interface{}, interface{}](10, 100)
+			p := NewPool[any, any](10, 100)
 			p.Submit(tt.args.handler, tt.args.args)
 		})
 	}
@@ -86,8 +87,8 @@ func TestPool_Submit(t *testing.T) {
 
 func TestPool_AsyncSubmit(t *testing.T) {
 	type args struct {
-		handler func(interface{}) interface{}
-		args    interface{}
+		handler func(any) any
+		args    any
 	}
 	tests := []struct {
 		name string
@@ -96,7 +97,7 @@ func TestPool_AsyncSubmit(t *testing.T) {
 		{
 			name: "test",
 			args: args{
-				handler: func(arg interface{}) interface{} {
+				handler: func(arg any) any {
 					for k := 0; k < 100; k++ {
 						_ = k
 					}
@@ -107,15 +108,15 @@ func TestPool_AsyncSubmit(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			p := NewPool[interface{}, interface{}](10, 100)
+			p := NewPool[any, any](10, 100)
 			p.AsyncSubmit(tt.args.handler, tt.args.args)
 		})
 	}
 }
 func TestPool_Map(t *testing.T) {
 	type args struct {
-		handler func(interface{}) interface{}
-		args    []interface{}
+		handler func(any) any
+		args    []any
 	}
 	tests := []struct {
 		name string
@@ -124,21 +125,65 @@ func TestPool_Map(t *testing.T) {
 		{
 			name: "test",
 			args: args{
-				handler: func(arg interface{}) interface{} {
+				handler: func(arg any) any {
 					for k := 0; k < 100; k++ {
 						_ = k
 					}
 					return nil
 				},
-				args: []interface{}{nil, nil, nil, nil, nil, nil, nil, nil, nil, nil},
+				args: []any{nil, nil, nil, nil, nil, nil, nil, nil, nil, nil},
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			p := NewPool[interface{}, interface{}](10, 100)
+			p := NewPool[any, any](10, 100)
 			defer p.Close()
 			p.Map(tt.args.handler, tt.args.args)
+		})
+	}
+}
+
+func TestNewPool(t *testing.T) {
+	type args struct {
+		numWorkers int
+		cap        int
+	}
+	type testCase[A any, R any] struct {
+		name string
+		args args
+		want *Pool[A, R]
+	}
+	tests := []testCase[any, any]{
+		{
+			name: "test_num_workers_0",
+			args: args{
+				numWorkers: 0,
+				cap:        100,
+			},
+			want: &Pool[any, any]{
+				numWorkers: runtime.NumCPU(),
+				taskChan:   make(chan task[any, any], 100),
+			},
+		},
+		{
+			name: "test_cap_0",
+			args: args{
+				numWorkers: 10,
+				cap:        0,
+			},
+			want: &Pool[any, any]{
+				numWorkers: 10,
+				taskChan:   make(chan task[any, any], 20),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewPool[any, any](tt.args.numWorkers, tt.args.cap); cap(got.taskChan) != cap(tt.want.taskChan) ||
+				got.numWorkers != tt.want.numWorkers {
+				t.Errorf("NewPool() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
